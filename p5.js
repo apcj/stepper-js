bedb8a0eff9e603b36c114f187d9468087ef30a8
Alistair Jones <alistair.jones@gmail.com>
2011-12-29 17:32:59 +0000

    Animate steps using d3.transition()

--- p5.js
+++ p5.js
@@ -24,10 +24,16 @@ p5.step = function() {
   };
   
   step.animate = function(selection) {
+    var duration = 500;
+    step.duration = function(overrideDuration) {
+      duration = overrideDuration;
+      return step;
+    }
+    
     step.style = function(key, value) {
       step.forward = function() {
         var previousValue = selection.style(key);
-        selection.style(key, value);
+        selection.transition().duration(duration).style(key, value);
 
         step.back = function() {
           selection.style(key, previousValue);
@@ -47,7 +53,9 @@ d3.select(window).on("load", function() {
 
   p5.step().animate(block).style("width", "200px");
   p5.step().animate(block).style("width", "300px");
+  p5.step().animate(block).style("height", "300px");
   p5.step().animate(block).style("width", "500px");
+  p5.step().animate(block).duration(2000).style("height", "500px");
 });
 
 
