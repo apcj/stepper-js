7bd5a7385169bf55517c6e7d3c05c68886fffc95
Alistair Jones <alistair.jones@gmail.com>
2012-01-15 17:17:47 +0000

    Rename P5 to stepper

--- examples/types-of-database/types-of-database.js
+++ examples/types-of-database/types-of-database.js
@@ -3,6 +3,6 @@ d3.select(window).on("load", function() {
   var table = d3.select(".feature");
   
   for (var i = 1; i <= 5; i++) {
-    p5.step().animate(table.selectAll(".appear" + i)).duration(300).style("opacity", 1);
+    stepper.step().animate(table.selectAll(".appear" + i)).duration(300).style("opacity", 1);
   }
 });
